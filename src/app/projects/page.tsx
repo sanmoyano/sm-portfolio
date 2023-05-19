1559c6d0431f2718043de52db10fcf66d319a10e
sanmoyano <smoyano.di@gmail.com>
2023-05-19 18:19:35 -0300

    preparing projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,25 @@
+'use client'
+import React from 'react'
 import { NextPage } from 'next'
+import { VStack, Text } from '@chakra-ui/react'
+
+import PROJECTS from '../../data/projects.json'
+
+interface ProjectsData {
+  name:string
+  page:string
+  repo:string
+}
 
 const Projects : NextPage = () => {
+  const projects: ProjectsData[] = PROJECTS as ProjectsData[]
+
   return (
-    <div>Projects</div>
+    <VStack alignItems='center' height={window.screen.height} justifyContent='center'>
+      {projects?.map((project) => (
+        <Text key={project.page} fontFamily='apotek-extrawide' fontSize='9rem' lineHeight={0.7} textTransform='uppercase'>{project.name}</Text>
+      ))}
+    </VStack>
   )
 }
 

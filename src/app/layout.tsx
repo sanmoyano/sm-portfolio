bc6f8d92a5e1e8d470a427bbd9569ab86111dab8
sanmoyano <smoyano.di@gmail.com>
2023-05-22 18:03:52 -0300

    meta update image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout ({
           sizes='192x192'
           type='image/png'
         />
+        <meta content='images/favicon/favicon-96x96.png' property='og:image' />
         <link href='/apple-touch-icon.png' rel='apple-touch-icon' sizes='180x180' />
         <link href='/favicon-32x32.png' rel='icon' sizes='32x32' type='image/png' />
         <link href='/favicon-16x16.png' rel='icon' sizes='16x16' type='image/png' />

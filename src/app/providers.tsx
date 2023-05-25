cd04b30e8e8d7cc82e14b1a6e3bde0d56f04d8bf
sanmoyano <smoyano.di@gmail.com>
2023-05-25 20:44:18 -0300

    cache provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import { ParallaxProvider } from 'react-scroll-parallax'
-// import { CacheProvider } from '@chakra-ui/next-js'
+import { CacheProvider } from '@chakra-ui/next-js'
 
 import theme from '@/theme/theme'
 type ProvidersProps = {
@@ -11,14 +11,14 @@ type ProvidersProps = {
 const Providers: React.FC<ProvidersProps> = ({ children }) => {
   return (
     <>
-      {/* <CacheProvider> */}
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <ChakraProvider theme={theme}>
-        <ParallaxProvider>
-          {children}
-        </ParallaxProvider>
-      </ChakraProvider>
-      {/* </CacheProvider> */}
+      <CacheProvider>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <ChakraProvider theme={theme}>
+          <ParallaxProvider>
+            {children}
+          </ParallaxProvider>
+        </ChakraProvider>
+      </CacheProvider>
     </>
   )
 }

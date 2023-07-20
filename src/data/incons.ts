b97870a879172ecf790674a47a8fd57342edcf7d
Santi Moyano <smoyano.di@gmail.com>
2023-07-20 20:44:18 -0300

    Update incons.ts

--- src/data/incons.ts
+++ src/data/incons.ts
@@ -9,13 +9,5 @@ export const icons: { icon:IconDefinition; url:string }[] = [
   {
     icon: faGithub,
     url: 'https://github.com/sanmoyano'
-  },
-  {
-    icon: faTelegram,
-    url: 'https://t.me/+5493517562121'
-  },
-  {
-    icon: faWhatsapp,
-    url: 'https://wa.me/3517562121'
   }
 ]
